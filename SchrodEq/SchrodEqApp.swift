3b21b403e8aceab9e80270a201018ddcdb754277
Tim Stack <tstack43215@yahoo.com>
2023-02-24 15:48:47 -0600

    Initial Commit

---
+++ SchrodEq/SchrodEqApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchrodEqApp.swift
+//  SchrodEq
+//
+//  Created by IIT PHYS 440 on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SchrodEqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
